6f8b3142c844cd5ba6922dc2e7ce4bbfe51f13e0
ohyunji <93081185+hongdii@users.noreply.github.com>
2023-04-10 15:17:44 +0900

    0410 하는중

--- The_Growing/src/com/kh/board/model/dao/BoardDao.java
+++ The_Growing/src/com/kh/board/model/dao/BoardDao.java
@@ -485,7 +485,6 @@ public int deleteAttachment(Connection conn, int bno, int filelevel) {
 				b.setBoardNo(rset.getInt("BOARD_NO"));
 				b.setBoardTitle(rset.getString("BOARD_TITLE"));
 				b.setBoardContent(rset.getString("BOARD_CONTENT"));
-				b.setUserId(rset.getString("USER_ID"));
 				b.setCreateDate(rset.getDate("CREATE_DATE"));
 			}
 		} catch (SQLException e) {
