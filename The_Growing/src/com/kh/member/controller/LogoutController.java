1374215b0aba082e14f159613e9e236ee8632ccb
JunHaRyu <wnsgk1207@gmail.com>
2023-04-14 23:23:07 +0900

    update

--- The_Growing/src/com/kh/member/controller/LogoutController.java
+++ The_Growing/src/com/kh/member/controller/LogoutController.java
@@ -3,6 +3,7 @@ package com.kh.member.controller;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,7 +27,16 @@ public class LogoutController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		Cookie c1 = new Cookie("userId", "");
+		Cookie c2 = new Cookie("userPwd", "");
+		Cookie c3 = new Cookie("userLevel", "");
+		c1.setMaxAge(0);
+		c2.setMaxAge(0);
+		c3.setMaxAge(0);
+		response.addCookie(c1);
+		response.addCookie(c2);
+		response.addCookie(c3);
+		
 		request.getSession().invalidate();// 세션영역에 있는 데이터를 모두 날리는 함수
 	
 		response.sendRedirect(request.getContextPath());
